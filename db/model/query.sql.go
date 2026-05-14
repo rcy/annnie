0e48a4db876862c4ae437fd2f721ed84fe3f14f6
Ryan Yeske <ryan@ryanyeske.com>
2026-05-14 16:50:49 -0700

    Fix tablescan

--- db/model/query.sql.go
+++ db/model/query.sql.go
@@ -597,7 +597,7 @@ func (q *Queries) ListConfigs(ctx context.Context) ([]Config, error) {
 }
 
 const listFiles = `-- name: ListFiles :many
-select id, created_at, nick, mime from files order by created_at desc limit 10
+select id, created_at, nick, mime from files order by id desc limit 10
 `
 
 type ListFilesRow struct {
